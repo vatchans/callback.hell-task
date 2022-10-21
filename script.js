bb9c94e5b53207c42735e6c2fc5c37077b88a690
vatchans <112924038+vatchans@users.noreply.github.com>
2022-10-21 17:31:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+var contanier = document.getElementById('container')
+setTimeout(() => {
+    contanier.innerHTML = `<p>10</p>`;
+    setTimeout(() => {
+        contanier.innerHTML = `<p>9</p>`;
+        setTimeout(() => {
+            contanier.innerHTML = `<p>8</p>`;
+            setTimeout(() => {
+                contanier.innerHTML = `<p>7</p>`;
+                setTimeout(() => {
+                    contanier.innerHTML = `<p>6</p>`;
+                    setTimeout(() => {
+                        contanier.innerHTML = `<p>5</p>`;
+                        setTimeout(() => {
+                            contanier.innerHTML = `<p>4</p>`;
+                            setTimeout(() => {
+                                contanier.innerHTML = `<p>3</p>`;
+                                setTimeout(() => {
+                                    contanier.innerHTML = `<p>2</p>`;
+                                    setTimeout(() => {
+                                        contanier.innerHTML = `<p>1</p>`;
+                                        setTimeout(() => {
+                                            contanier.innerHTML = ` <img src="https://img.freepik.com/free-vector/happy-independence-day-banner-with-indian-flag_1017-20161.jpg?w=1380&t=st=1666352505~exp=1666353105~hmac=1fa2ab59a6aadb384448ab4f9438a4e3cc1effd26288cf7a953b43cf62966c94">`;
+                                        },1500);
+                                    },1500);
+                                },1500);
+                            },1500);
+                        },1500);
+                    }, 1500);
+                }, 1500);
+            }, 1500);
+        }, 1500);
+    }, 1500);
+},0);
+
+
+
